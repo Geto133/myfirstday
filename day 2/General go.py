22b67f1c62995386b382d08ec78e337b37c5b0e8
geto133 <getia.giorgi00@gmail.com>
2022-09-04 16:45:16 +0400

    first commit

---
+++ day 2/General go.py
@@ -0,0 +1,23 @@
+from ctypes.wintypes import SIZE
+from turtle import *
+
+bgcolor("red")
+
+range(5)
+forward(200)
+left(216)
+forward(200)
+left(216)
+forward(200)
+left(216)
+forward(200)
+left(216)
+forward(200)
+left(216)
+
+
+
+
+
+
+exitonclick()
